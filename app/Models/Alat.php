a3d96af2c7ecd35d373553d799d6748512ef4601
Rahmat tanjung <amektanjung2@gmail.com>
2026-06-01 12:35:33 +0700

    feat: menambahkan soft deletes pada model lapangan dan alat untuk melindungi integritas data riwayat pesanan

--- app/Models/Alat.php
+++ app/Models/Alat.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // 1. Tambahkan baris ini
 
 class Alat extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes; // 2. Tambahkan SoftDeletes di sebelah HasFactory
 
     protected $fillable = [
         'nama_alat',
